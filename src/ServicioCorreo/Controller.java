982ac2e98ea22db21b285afaeb2618ec881432f0
adarbell <rubrod@alu.iesserpis.org>
2020-01-16 08:43:21 +0100

    Clase Email

--- src/ServicioCorreo/Controller.java
+++ src/ServicioCorreo/Controller.java
@@ -1,4 +1,4 @@
-package sample;
+package ServicioCorreo;
 
 public class Controller {
 }
--- src/ServicioCorreo/Main.java
+++ src/ServicioCorreo/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package ServicioCorreo;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
