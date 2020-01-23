ec3a22bc0acba28fa97cf49490c35a0eb40eb8d3
sandra1036 <sandraromero262@gmail.com>
2020-01-23 09:36:25 +0100

    Añadido otro fxml

--- src/ServicioCorreo/Controller.java
+++ src/ServicioCorreo/Controller.java
@@ -1,13 +1,4 @@
-package ServicioCorreo;
+package sample;
 
-import  javafx.applicacion.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-public class Controller{
-
-
-
+public class Controller {
 }
